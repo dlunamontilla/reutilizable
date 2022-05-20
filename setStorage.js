87a956e582b56eb1636bcafc85ad3cea74640e3e
David E Luna M <davidlunamontilla@gmail.com>
2022-05-20 12:03:03 -0500

    Se agrega una función para almacenar datos en localStorage

---
+++ setStorage.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param { string } key Es el nombre de item de localStorage, es decir,
+ * el equivalente a definirlo así: localStorage.setItem(key, value) o también:
+ * localStorage.getItem(key).
+ * 
+ * @param { Object<string, string|number> } object Es el  objeto que queremos agragar en
+ * localStorage.
+ * 
+ * @returns { void }
+ */
+function setStorage(key, object) {
+    if (!object || typeof key !== "string") return;
+
+    const dataString = localStorage.getItem(key);
+    const data = [];
+
+    if (dataString) {
+        data.push(...JSON.parse(dataString));
+    }
+
+    const { length } = data;
+    data.push({ id: length + 1, ...object });
+
+    localStorage.setItem(key, JSON.stringify(data));
+}
